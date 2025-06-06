85e05f497fafcf99309109b73d3f42a4fa3dfdb1
Jordi Ruiz <jordiruizroman21@gmail.com>
2025-06-06 16:14:35 +0200

    Corrijo Dockerfile y añado gunicorn

--- proyectoDjango/proyectoDjango/settings.py
+++ proyectoDjango/proyectoDjango/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'adfñakjdñakd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
